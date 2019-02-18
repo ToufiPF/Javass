907679bacc0f0c7253d3f6117c5307a4d1975050
Unknown <aureli1.clergeot@laposte.net>
2019-02-18 14:37:42 +0100

    Preconditions - Fini
    Documentation
    checkArgument
    checkIndex

--- src/ch/epfl/javass/Preconditions.java
+++ src/ch/epfl/javass/Preconditions.java
@@ -1,29 +1,31 @@
 package ch.epfl.javass;
 
-
 /**
- * Preconditions
- * Une classe non instanciable qui fournit des méthodes
- * pour vérifier la validité d'un argument
+ * Preconditions Une classe non instanciable qui fournit des méthodes pour
+ * vérifier la validité d'un argument
+ * 
  * @author Aurélien Clergeot (302592)
  */
 public final class Preconditions {
-	private Preconditions() {};
-	
+	private Preconditions() {
+	};
+
 	/**
-	 * 
-	 * @param arg
+	 * @param b (boolean) l'argument à vérifier
+	 * @throws IllegalArgumentException quand b vaut false
 	 */
-	public static void checkArguments(boolean arg) {
-		if (!arg)
+	public static void checkArgument(boolean b) throws IllegalArgumentException {
+		if (!b)
 			throw new IllegalArgumentException();
 	}
+
 	/**
-	 * @param index
-	 * @param size
-	 * @return
+	 * @param index (int) l'index à vérifier
+	 * @param size (int) la taille à ne pas dépasser
+	 * @return (int) index, non modifié
+	 * @throws IndexOutOfBoundsException quand index < 0 ou index >= size
 	 */
-	public static int checkIndex(int index, int size) {
+	public static int checkIndex(int index, int size) throws IndexOutOfBoundsException {
 		if (index < 0 || index >= size)
 			throw new IndexOutOfBoundsException();
 		return index;
