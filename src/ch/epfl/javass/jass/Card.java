2d73046a69ae1921927c933666939fe5cb1a0dd7
Unknown <amaury.pierre@epfl.ch>
2019-02-18 22:32:58 +0100

    Classes Jass et Card
    C'etait rapide

---
+++ src/ch/epfl/javass/jass/Card.java
@@ -0,0 +1,9 @@
+/*
+ * Auteur : Amaury Pierre
+ * Date :   18 févr. 2019
+ */
+package ch.epfl.javass.jass;
+
+public final class Card {
+
+}
---
+++ src/ch/epfl/javass/jass/Jass.java
@@ -0,0 +1,13 @@
+/*
+ * Auteur : Amaury Pierre
+ * Date :   18 févr. 2019
+ */
+package ch.epfl.javass.jass;
+
+public interface Jass {
+    public final static int HAND_SIZE = 9;
+    public final static int TRICKS_PER_TURN = 9;
+    public final static int WINNING_POINT = 1000;
+    public final static int MATCH_ADDITIONNAL_POINT = 100;
+    public final static int LAST_TRICK_ADDITIONNAL_POINTS = 5;
+}
