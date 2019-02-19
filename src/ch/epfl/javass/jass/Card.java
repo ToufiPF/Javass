e7f11f46a74d3e456a02167915f801ed837dc260
Unknown <amaury.pierre@epfl.ch>
2019-02-19 14:05:14 +0100

    Classe Card.Color

--- src/ch/epfl/javass/jass/Card.java
+++ src/ch/epfl/javass/jass/Card.java
@@ -4,6 +4,31 @@
  */
 package ch.epfl.javass.jass;
 
-public final class Card {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public final class Card {
+    /**
+     * Classe imbriquées énumérant les couleur des cartes
+     */
+    public enum Color {
+        SPADE("\u2660"), 
+        HEART("\u2661"), 
+        DIAMOND("\u2662"), 
+        CLUB("\u2663");
+
+        private String symbol;
+        public static final List<Color> ALL = Collections.unmodifiableList(Arrays.asList(values()));
+        private static final int COUNT = 4;
+        
+        private Color(String symbol) {
+            this.symbol = symbol;
+        }
+        
+        public String toString() {
+            return symbol;
+        }
+        
+    }
 }
