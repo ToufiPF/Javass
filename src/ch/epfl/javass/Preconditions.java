1674062a19c1ee1a425a7d78dfb23f0832e83d46
Unknown <aureli1.clergeot@laposte.net>
2019-02-19 14:43:49 +0100

    Doc Modifiée + Utilisation Preconditions
    Doc modifiée dans Bit32 et Preconditions
    + Utilisation de Preconditions.checkArgument
    au lieu de if(...){throw Exception} dans Bits32

--- src/ch/epfl/javass/Preconditions.java
+++ src/ch/epfl/javass/Preconditions.java
@@ -1,33 +1,41 @@
 package ch.epfl.javass;
 
 /**
- * Preconditions Une classe non instanciable qui fournit des méthodes pour
+ * Preconditions 
+ * Une classe non instanciable qui fournit des méthodes pour
  * vérifier la validité d'un argument
  * 
  * @author Aurélien Clergeot (302592)
  */
 public final class Preconditions {
-	private Preconditions() {
-	};
+    private Preconditions() {
+    };
 
-	/**
-	 * @param b (boolean) l'argument à vérifier
-	 * @throws IllegalArgumentException quand b vaut false
+    /**
+     * Methode utilitaire qui lance une exception
+     * si l'argument passé vaut false
+     * 
+     * @param b (boolean) l'argument à vérifier
+     * @throws IllegalArgumentException quand b vaut false
      */
-	public static void checkArgument(boolean b) throws IllegalArgumentException {
-		if (!b)
-			throw new IllegalArgumentException();
-	}
+    public static void checkArgument(boolean b) throws IllegalArgumentException {
+        if (!b)
+            throw new IllegalArgumentException();
+    }
 
-	/**
-	 * @param index (int) l'index à vérifier
-	 * @param size (int) la taille à ne pas dépasser
-	 * @return (int) index, non modifié
-	 * @throws IndexOutOfBoundsException quand index < 0 ou index >= size
-	 */
-	public static int checkIndex(int index, int size) throws IndexOutOfBoundsException {
-		if (index < 0 || index >= size)
-			throw new IndexOutOfBoundsException();
-		return index;
-	}
+    /**
+     * Methode utilitaire qui vérifie la validité d'un index
+     * càd index >= 0, et index < size.
+     * Lance un IndexOutOfBoundsException sinon
+     * 
+     * @param index (int) l'index à vérifier
+     * @param size (int) la taille à ne pas dépasser
+     * @return (int) index, non modifié
+     * @throws IndexOutOfBoundsException quand index < 0 ou index >= size
+     */
+    public static int checkIndex(int index, int size) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        return index;
+    }
 }
--- src/ch/epfl/javass/bits/Bits32.java
+++ src/ch/epfl/javass/bits/Bits32.java
@@ -1,28 +1,36 @@
 package ch.epfl.javass.bits;
 
+import ch.epfl.javass.Preconditions;
+
+/**
+ * Bits32 
+ * Une classe non instanciable contenant des méthodes permettant de
+ * travailler sur des vecteurs de 32 bits
+ * 
+ * @author Amaury Pierre 
+ */
 public final class Bits32 {
 
-    /**
-     * Bits32 Une classe non instanciable contenant des méthodes permettant de
-     * travailler sur des vecteurs de 32 bits
-     */
     private Bits32() {
     };
 
     /**
+     * Crée un masque sur un int,
+     * de taille size, et commençant au start-ième bit
+     * Attention : le 1er bit est à start = 0
+     * 
      * @param start
      *            (int) l'emplacement du bit de départ
      * @param size
      *            (int) la taille du masque à créer
      * @return (int) un masque de taille size et de bit de départ start
      * @throws IllegalArgumentException
-     *             lorsque start et size ne désignent pas un plage valide
+     *             lorsque start et size ne désignent pas une plage valide
      */
     public static int mask(int start, int size)
             throws IllegalArgumentException {
-        if (start < 0 || start + size >= Integer.SIZE || size < 0) {
-            throw new IllegalArgumentException();
-        }
+        
+        Preconditions.checkArgument((start < 0 || size < 0 || start + size >= Integer.SIZE));
 
         int entier = 0;
         for (int i = start; i < start + size; ++i) {
@@ -32,6 +40,9 @@ public final class Bits32 {
     }
 
     /**
+     * Extrait de bits l'entier composé par l'ensemble des bits
+     * commençant en start et de taille size
+     * 
      * @param bits
      *            (int) la chaîne de bits de départ
      * @param start
@@ -49,38 +60,29 @@ public final class Bits32 {
     }
 
     /**
-     * Méthode utilitaire permettant de vérifier si un couple d'entiers est
-     * valide
+     * Permet de packer deux entiers en un seul
+     * en collant v1 sur les s1 bits de poids faible,
+     * et v2 sur les s2 suivants
      * 
-     * @throws IllegalArgumentException
-     *             si s1 n'est pas valide ou si la représentation en bits de v1
-     *             est de taille supérieure à s1
-     */
-    private static void check(int v1, int s1) throws IllegalArgumentException {
-        if ((s1 >= Integer.SIZE) || (s1 < 0) || (v1 >= Math.pow(2, s1))) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    /**
-     * @param v1
-     *            (int)
-     * @param s1
-     *            (int)
-     * @param v2
-     *            (int)
+     * @param v1 
+     *            (int) entier n°1 à packer dans l'entier retourné
+     * @param s1 
+     *            (int) nombre de bits alloués pour packer v1
+     * @param v2 
+     *            (int) entier n°2 à packer
      * @param s2
-     *            (int)
-     * @return un entier composé de v1 occupant les s1 bits de poids faible et
+     *            (int) nombre de bits alloués pour packer v2
+     *            
+     * @return (int) un entier composé de v1 occupant les s1 bits de poids faible et
      *         de v2 occupant les s2 bits suivants
      * @throws IllegalArgumentException
-     *             si la somme de s1 et s2 dépasse la taille d'un int
+     *             si la somme de s1 et s2 dépasse la taille d'un int,
+     *             ou si v1/v2 ne sont pas representables en s1/s2 bits
      */
     public static int pack(int v1, int s1, int v2, int s2)
             throws IllegalArgumentException {
-        if ((s1 + s2) >= Integer.SIZE) {
-            throw new IllegalArgumentException();
-        }
+        
+        Preconditions.checkArgument((s1 + s2) >= Integer.SIZE);
         check(v1, s1);
         check(v2, s2);
 
@@ -93,18 +95,31 @@ public final class Bits32 {
     /**
      * Surcharge de pack fonctionnant pour 3 chiffres
      * 
-     * @return une entier composé de v1 occupant les s1 bits de poids faible, de
+     * @param v1 
+     *            (int) entier n°1 à packer dans l'entier retourné
+     * @param s1 
+     *            (int) nombre de bits alloués pour packer v1
+     * @param v2 
+     *            (int) entier n°2 à packer
+     * @param s2
+     *            (int) nombre de bits alloués pour packer v2
+     * @param v3
+     *            (int) entier n°3 à packer
+     * @param s3
+     *            (int) nombre de bits alloués pour packer v3
+     *            
+     * @return (int) un entier composé de v1 occupant les s1 bits de poids faible, de
      *         v2 occupant les s2 bits suivants et de v3 occupant les s3 bits
      *         suivants
      * @throws IllegalArgumentException
      *             si la taille totale de l'entier créé est supérieure à la
-     *             taille d'un int
+     *             taille d'un int, ou si les entiers vI donnés ne sont pas
+     *             représentables par le nombre de bits sI correspondant
      */
     public static int pack(int v1, int s1, int v2, int s2, int v3, int s3)
             throws IllegalArgumentException {
-        if ((s1 + s2 + s3) >= Integer.SIZE) {
-            throw new IllegalArgumentException();
-        }
+        
+        Preconditions.checkArgument( (s1 + s2 + s3) >= Integer.SIZE);
 
         check(v3, s3);
 
@@ -114,21 +129,21 @@ public final class Bits32 {
     }
 
     /**
-     * Surcharge de pack fonctionnant pour 3 chiffres
+     * Surcharge de pack fonctionnant pour 7 chiffres
      * 
-     * @return une entier composé de v1 occupant les s1 bits de poids faible, de
+     * @return (int) un entier composé de v1 occupant les s1 bits de poids faible, de
      *         v2 occupant les s2 bits suivants, de v3 occupant les s3 bits
      *         suivants, etc.
      * @throws IllegalArgumentException
      *             si la taille totale de l'entier créé est supérieure à la
-     *             taille d'un int
+     *             taille d'un int, ou si les entiers vI donnés ne sont pas
+     *             représentables par le nombre de bits sI correspondant
      */
     public static int pack(int v1, int s1, int v2, int s2, int v3, int s3,
             int v4, int s4, int v5, int s5, int v6, int s6, int v7, int s7)
-            throws IllegalArgumentException {
-        if (s1 + s2 + s3 + s4 + s5 + s6 + s7 >= Integer.SIZE) {
-            throw new IllegalArgumentException();
-        }
+                    throws IllegalArgumentException {
+        
+        Preconditions.checkArgument( (s1 + s2 + s3 + s4 + s5 + s6 + s7) >= Integer.SIZE);
 
         int entier1 = pack(v1, s1, v2, s2, v3, s3);
         int entier2 = pack(v4, s4, v5, s5);
@@ -137,4 +152,18 @@ public final class Bits32 {
         return entier1 | (entier2 << (s1 + s2 + s3))
                 | (entier3 << (s1 + s2 + s3 + s4 + s5));
     }
+    
+
+    /**
+     * Méthode utilitaire permettant de vérifier si un couple d'entiers est
+     * valide
+     * 
+     * @throws IllegalArgumentException
+     *             si s1 n'est pas valide ou si la représentation en bits de v1
+     *             est de taille supérieure à s1
+     */
+    private static void check(int v1, int s1) throws IllegalArgumentException {
+        Preconditions.checkArgument((s1 >= Integer.SIZE) || (s1 < 0) || (v1 >= Math.pow(2, s1)));
+    }
+
 }
