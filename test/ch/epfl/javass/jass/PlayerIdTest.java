c3bce6ccc9cf4874826dc6039fba0726e291c4ca
Unknown <aureli1.clergeot@laposte.net>
2019-02-22 14:39:25 +0100

    PlayerIDTest

---
+++ test/ch/epfl/javass/jass/PlayerIdTest.java
@@ -0,0 +1,37 @@
+package ch.epfl.javass.jass;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerIdTest {
+    
+    private static PlayerId[] getAllPlayers() {
+        return new PlayerId[] {
+          PlayerId.PLAYER_1,
+          PlayerId.PLAYER_2,
+          PlayerId.PLAYER_3,
+          PlayerId.PLAYER_4
+        };
+    }
+    
+    @Test
+    void ALLisInRightOrder() {
+        assertArrayEquals(getAllPlayers(), PlayerId.values());
+    }
+    
+    @Test
+    void countIsGood() {
+        assertEquals(4, PlayerId.COUNT);
+        assertEquals(PlayerId.values().length, PlayerId.COUNT);
+    }
+    
+    @Test
+    void teamReturnsRightTeam() {
+        assertEquals(TeamId.TEAM_1, PlayerId.PLAYER_1.team());
+        assertEquals(TeamId.TEAM_2, PlayerId.PLAYER_2.team());
+        assertEquals(TeamId.TEAM_1, PlayerId.PLAYER_1.team());
+        assertEquals(TeamId.TEAM_2, PlayerId.PLAYER_2.team());
+    }
+
+}
