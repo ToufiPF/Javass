d0b26fecd3724055c039b5b04b37fb07f0ddca7d
Unknown <aureli1.clergeot@laposte.net>
2019-02-22 15:17:35 +0100

    RandomGenerators packedScore

--- test/ch/epfl/javass/jass/PackedScoreTest.java
+++ test/ch/epfl/javass/jass/PackedScoreTest.java
@@ -8,8 +8,11 @@ import java.util.SplittableRandom;
 
 import org.junit.jupiter.api.Test;
 
+import ch.epfl.javass.jass.PackedScore;
+import ch.epfl.javass.jass.Score;
+
 class PackedScoreTest {
-    private long getRandomValidPackedScore(SplittableRandom generator) {
+    public static long getRandomValidPackedScore(SplittableRandom generator) {
 
         int tricks1 = generator.nextInt(9);
         int turnPoints1 = generator.nextInt(257);
@@ -22,8 +25,36 @@ class PackedScoreTest {
         return PackedScore.pack(tricks1, turnPoints1, gamePoints1, tricks2,
                 turnPoints2, gamePoints2);
     }
+    public static long getRandomPackedScoreWithTurnTricks(int turnTricks1, int turnTricks2, SplittableRandom rng) {
+        int turnPoints1 = rng.nextInt(257);
+        int gamePoints1 = rng.nextInt(2000);
+        int turnPoints2 = rng.nextInt(257 - turnPoints1);
+        int gamePoints2 = rng.nextInt(2000);
+        return PackedScore.pack(turnTricks1, turnPoints1, gamePoints1, turnTricks2, turnPoints2, gamePoints2);
+    }
+    
+    public static long getRandomPackedScoreWithTurnPoints(int turnPoints1, int turnPoints2, SplittableRandom rng) {
+        int turnTricks1 = rng.nextInt(9);
+        int gamePoints1 = rng.nextInt(2000);
+        int turnTricks2 = rng.nextInt(9 - turnTricks1);
+        int gamePoints2 = rng.nextInt(2000);
+        
+        return PackedScore.pack(turnTricks1, turnPoints1, gamePoints1, turnTricks2, turnPoints2, gamePoints2);
+    }
+    
+    public static long getRandomPackedScoreWithGamePoints(int gamePoints1, int gamePoints2, SplittableRandom rng) {
 
-    private long getRandomInvalidPackedScore(SplittableRandom generator) {
+        int tricks1 = rng.nextInt(9);
+        int turnPoints1 = rng.nextInt(257);
+
+        int tricks2 = rng.nextInt(9 - tricks1);
+        int turnPoints2 = rng.nextInt(257 - turnPoints1);
+
+        return PackedScore.pack(tricks1, turnPoints1, gamePoints1, tricks2,
+                turnPoints2, gamePoints2);
+    }
+
+    private static long getRandomInvalidPackedScore(SplittableRandom generator) {
 
         int tricks1 = generator.nextInt(6) + 10;
         int turnPoints1 = generator.nextInt(255) + 258;
