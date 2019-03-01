0d3df667a339c886e6c0e53d6a27d1fdf0b1ac2a
Unknown <amaury.pierre@epfl.ch>
2019-03-01 13:48:30 +0100

    Tests complets PackedCardSet

--- test/ch/epfl/javass/jass/PackedCardSetTest.java
+++ test/ch/epfl/javass/jass/PackedCardSetTest.java
@@ -3,6 +3,7 @@ package ch.epfl.javass.jass;
 import static ch.epfl.test.TestRandomizer.RANDOM_ITERATIONS;
 import static ch.epfl.test.TestRandomizer.newRandom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,6 +13,8 @@ import java.util.SplittableRandom;
 
 import org.junit.jupiter.api.Test;
 
+import ch.epfl.javass.bits.Bits32;
+import ch.epfl.javass.bits.Bits64;
 import ch.epfl.javass.jass.Card.Color;
 import ch.epfl.javass.jass.Card.Rank;
 
@@ -120,6 +123,36 @@ class PackedCardSetTest {
                 }
                 return true;
             });
+        }
+    }
+    
+    @Test
+    void isValidWorksWithAllValidNumbers() {
+        for (int i = 0; i < (1L << 9); ++i) {
+            for (int j = 0; j < (1L << 9); ++j) {
+                for (int k = 0; k < (1L << 9); ++k) {
+                    for (int l = 0; l < (1L << 9); ++l) {
+                        int m = Bits32.pack(i, 16, j, 16);
+                        int n = Bits32.pack(k, 16, l, 16);
+                        long o = Bits64.pack(m, 32, n, 32);
+                        assertTrue(PackedCardSet.isValid(o));
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    void isValidFailsWithAllUnvalidNumbers() {
+        for (int i = 1; i < (1L << 7); ++i) {
+            for (int j = 1; j < (1L << 7); ++j) {
+                for (int k = 1; k < (1L << 7); ++k) {
+                    for (int l = 1; l < (1L << 7); ++l) {
+                        long m = (l << 57 | k << 41 | j << 25 | i << 9);
+                        assertFalse(PackedCardSet.isValid(m));
+                    }
+                }
+            }
         }
     }
 }
