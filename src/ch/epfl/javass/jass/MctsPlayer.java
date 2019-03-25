c01bbcc1b24faad4f0daac0168c7be381c343c0c
Unknown <aureli1.clergeot@laposte.net>
2019-03-25 13:35:56 +0100

    Constructeur de base MctsPlayer

--- src/ch/epfl/javass/jass/MctsPlayer.java
+++ src/ch/epfl/javass/jass/MctsPlayer.java
@@ -1,5 +1,9 @@
 package ch.epfl.javass.jass;
 
+import java.util.SplittableRandom;
+
+import ch.epfl.javass.Preconditions;
+
 /**
  * MctsPlayer Une classe publique et finale représentant un joueur
  *  simulé au moyen de l'algorithme MCTS
@@ -9,6 +13,17 @@ package ch.epfl.javass.jass;
  */
 public final class MctsPlayer implements Player {
     //TODO choose best class and put it here
+
+    private final PlayerId mOwnId;
+    private final int mIterations;
+    private final SplittableRandom mRng;
+
+    public MctsPlayer(PlayerId ownId, long rngSeed, int iterations) {
+        Preconditions.checkArgument(iterations >= 9);
+        mOwnId = ownId;
+        mRng = new SplittableRandom(rngSeed);
+        mIterations = iterations;
+    }
     @Override
     public Card cardToPlay(TurnState state, CardSet hand) {
         return null;
