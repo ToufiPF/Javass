60906daa6f604f05cf0dfb95a2abe6de7014028d
Unknown <aureli1.clergeot@laposte.net>
2019-03-27 11:11:43 +0100

    MctsGame

--- test/ch/epfl/javass/gametest/MctsJassGame.java
+++ test/ch/epfl/javass/gametest/MctsJassGame.java
@@ -54,34 +54,4 @@ public final class MctsJassGame {
         System.out.println("Temps écoulé (Algo terminal) : " + tempsMoyen / 1000.f + "s.");
         System.out.println(nbParties);
     }
-    
-
-    /*
-    public static void main(String[] args) {
-        TurnState state = TurnState.initial(Card.Color.SPADE, Score.INITIAL, PlayerId.PLAYER_1);
-        state = state.withNewCardPlayed(Card.of(Card.Color.SPADE, Card.Rank.JACK));
-
-        CardSet hand = CardSet.EMPTY
-                .add(Card.of(Card.Color.SPADE, Card.Rank.EIGHT))
-                .add(Card.of(Card.Color.SPADE, Card.Rank.NINE))
-                .add(Card.of(Card.Color.SPADE, Card.Rank.TEN))
-                .add(Card.of(Card.Color.HEART, Card.Rank.SIX))
-                .add(Card.of(Card.Color.HEART, Card.Rank.SEVEN))
-                .add(Card.of(Card.Color.HEART, Card.Rank.EIGHT))
-                .add(Card.of(Card.Color.HEART, Card.Rank.NINE))
-                .add(Card.of(Card.Color.HEART, Card.Rank.TEN))
-                .add(Card.of(Card.Color.HEART, Card.Rank.JACK));
-
-        MctsPlayer player2 = new MctsPlayer(PlayerId.PLAYER_2, 0, 100_000);
-        final long startTime = System.currentTimeMillis();
-
-        Card playedCard = player2.cardToPlay(state, hand);
-        final long endTime = System.currentTimeMillis();
-        long tempsMoyen = endTime - startTime;
-        System.out.println(tempsMoyen/1000.f);
-        System.out.println(playedCard);
-        //assertEquals(Card.of(Card.Color.SPADE, Card.Rank.EIGHT), playedCard);
-    }
-    */
-
 }
