6ffd29d0911df783508d1d7a31c91231f471c703
Unknown <aureli1.clergeot@laposte.net>
2019-04-08 16:52:54 +0200

    HandBean

--- src/ch/epfl/javass/gui/HandBean.java
+++ src/ch/epfl/javass/gui/HandBean.java
@@ -3,35 +3,44 @@ package ch.epfl.javass.gui;
 import ch.epfl.javass.jass.Card;
 import ch.epfl.javass.jass.CardSet;
 import ch.epfl.javass.jass.Jass;
-import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleSetProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableSet;
 
 public final class HandBean {
-    private ObservableList<Card> hand = new SimpleListProperty<>();
+    private Card[] hand = new Card[Jass.HAND_SIZE];
     private ObservableSet<Card> playableCards = new SimpleSetProperty<>();
     
     public ObservableList<Card> hand() {
-        return FXCollections.unmodifiableObservableList(hand);
+        return FXCollections.unmodifiableObservableList(FXCollections.observableArrayList(hand));
+    }
+    public void setHand(CardSet newHand) throws IllegalArgumentException {
+        final int handSize = newHand.size();
+        if(handSize > Jass.HAND_SIZE) {
+            throw new IllegalArgumentException("Nouvelle main trop grande.");
+        }
+        else if (handSize == Jass.HAND_SIZE) {
+            for(int i = 0; i < handSize ; ++ i)
+                hand[i] = newHand.get(i);
+        }
+        else if (handSize < Jass.HAND_SIZE) {
+            for(int i = 0; i < Jass.HAND_SIZE; ++ i)
+                if(!newHand.contains(hand[i]))
+                    hand[i] = null;
+        }
     }
     
-    public void setHand(CardSet newHand) throws IllegalArgumentException {
-        if(newHand.size() > Jass.HAND_SIZE) {
-            throw new IllegalArgumentException("Nouvelle main trop grande");
+    public ObservableSet<Card> playableCards() {
+        return FXCollections.unmodifiableObservableSet(playableCards);
+    }
+    public void setPlayableCards(CardSet newPlayableCards) {
+        final int playableSize = newPlayableCards.size();
+        if(playableSize > Jass.HAND_SIZE) {
+            throw new IllegalArgumentException("Trop de cartes jouables.");
         }
-        else if (newHand.size() == Jass.HAND_SIZE) {
-            hand.clear();
-            for(int i = 0; i < newHand.size(); ++ i)
-                hand.add(newHand.get(i));
-        }
-        else if (newHand.size() < Jass.HAND_SIZE) {
-            for(int i = 0; i < hand.size(); ++ i) {
-                if(!newHand.contains(hand.get(i))) {
-                    hand.set(i, null);
-                }
-            }
-        }
+        playableCards.clear();
+        for (int i = 0 ; i < playableSize ; ++i)
+            playableCards.add(newPlayableCards.get(i));
     }
 }
