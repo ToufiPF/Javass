1ba30479b39a63ac71383370886342d0963d1f5d
Unknown <aureli1.clergeot@laposte.net>
2019-05-06 14:12:38 +0200

    LocalMain params défaut

--- src/ch/epfl/javass/LocalMain.java
+++ src/ch/epfl/javass/LocalMain.java
@@ -7,6 +7,16 @@ import javafx.stage.Stage;
 
 public class LocalMain extends Application {
     
+    public static final double WAIT_TIME_MCTS_PLAYER = 2.;
+    
+    public static final double WAIT_TIME_TRICK_END = 1.;
+    
+    public static final String[] DEFAULT_NAMES = {"Aline", "Bastien", "Colette", "David" };
+    
+    public static final int DEFAULT_ITERATIONS = 10_000;
+    
+    public static final String DEFAULT_IP = "localhost";
+    
     public static void main(String[] args) {
         launch(args);
     }
@@ -15,14 +25,16 @@ public class LocalMain extends Application {
     public void start(Stage arg0) throws Exception {
         List<String> args = getParameters().getRaw();
         if (!(args.size() == 4 || args.size() == 5)) {
-            displayHelp();
+            System.err.println("Erreur : nombre d'arguments invalide");
+            System.err.println(getHelpMessage());
             return;
         }
         
     }
     
-    private void displayHelp() {
-        StringBuilder b = new StringBuilder("Utilisation : java ch.epfl.javass.LocalMain <j1>...<j4> [<graine>]\n")
+    private static String getHelpMessage() {
+        StringBuilder b = new StringBuilder()
+                .append("Utilisation : java ch.epfl.javass.LocalMain <j1>...<j4> [<graine>]\n")
                 .append("où :\n").append("<jn> spécifie le joueur n ainsi :\n")
                 .append("  h:<nom>               -> un joueur humain, nommé <nom>\n")
                 .append("  s:<nom>:<iterations>  -> un joueur simulé, nommé <nom> avec <iterations> itérations\n")
@@ -30,6 +42,6 @@ public class LocalMain extends Application {
                 .append("Remarque : les champs <nom>, <graine>, et <adresse> peuvent être omis dans les \n")
                 .append("arguments ci-dessus, ils sont alors remplacés par leur valeur par défaut (voir doc).\n")
                 .append("[<graine>] (optionnel) la graine utilisée pour générer la partie");
-        System.out.println(b.toString());
+        return b.toString();
     }
 }
