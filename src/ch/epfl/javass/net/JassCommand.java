4ddd7f5b1e8be932311e455cee9e54d785c5d69b
Unknown <amaury.pierre@epfl.ch>
2019-05-11 10:54:47 +0200

    Choix du trump en début de partie (quasi fini)

--- src/ch/epfl/javass/net/JassCommand.java
+++ src/ch/epfl/javass/net/JassCommand.java
@@ -8,7 +8,7 @@ package ch.epfl.javass.net;
  * @author Aurélien Clergeot (302592)
  */
 public enum JassCommand {
-    SET_PLAYERS("PLRS"), SET_TRUMP("TRMP"), SET_WINNING_TEAM(
+    SET_PLAYERS("PLRS"),CHOOSE_TRUMP("CHOS"), SET_TRUMP("TRMP"), SET_WINNING_TEAM(
             "WINR"), UPDATE_HAND("HAND"), UPDATE_TRICK(
                     "TRCK"), UPDATE_SCORE("SCOR"), CARD_TO_PLAY("CARD");
 
